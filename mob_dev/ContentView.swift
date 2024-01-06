9e8d751b747b2308bf10fc08b2804d67e18804e3
Sergei Pshonnov <43892086+kkeigenkai@users.noreply.github.com>
2024-01-06 22:56:14 +0300

    Initial Commit

---
+++ mob_dev/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mob_dev
+//
+//  Created by Sergei Pshonnov on 06.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mob_dev/mob_devApp.swift
@@ -0,0 +1,17 @@
+//
+//  mob_devApp.swift
+//  mob_dev
+//
+//  Created by Sergei Pshonnov on 06.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct mob_devApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
